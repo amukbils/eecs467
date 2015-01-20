fa6367011557ad278ae43cac666e56b222a56bdc
whitneyeecs <smithwe@umich.edu>
2015-01-20 15:34:27 -0500

    Camera stuff

--- src/apps/maebot_hello_world.c
+++ src/apps/maebot_hello_world.c
@@ -40,14 +40,20 @@
 #include "imagesource/image_convert.h"
 #include "eecs467_util.h" // This is where a lot of the internals live
 
+//camera
+#include <gtk/gtk.h>
+#include <gdk/gdkkeysyms.h>
+
+#include "image_u8x3.h"
+#include "image_source.h"
+#include "image_convert.h"
+#include "pnm.h"
+
 #define MAX_REVERSE_SPEED -1.0f
 #define MAX_FORWARD_SPEED 1.0f
 #define CMD_PRD 50000 //us -> 20Hz
 #define MTR_SPD 0.25f
 #define MTR_STOP 0.0f
-
-
-
 
 
 typedef struct state state_t;
@@ -60,6 +66,14 @@ struct state {
     lcm_t *lcm;
     const char *laser_channel;
     const char *corner_laser_channel;
+
+	//camera
+	char *url; //image_source url
+	image_source_t *isrc;
+
+	GtkWidget *window;
+	GtkWidget *image;	
+
     // image stuff
 //    char *img_url;
 //    int img_height;
@@ -75,10 +89,13 @@ struct state {
     //pthread_t animate_thread;
     pthread_t lcm_thread;
     pthread_t drive_square_thread;
+	pthread_t cam_thread;
 
     pthread_mutex_t laser_mutex;
     pthread_mutex_t move_mutex;
-//    maebot_laser_scan_t *laser_info;
+	pthread_mutex_t cam_mutex;
+
+	//maebot_laser_scan_t *laser_info;
     bool need_scan;
 
     // bot info
@@ -323,18 +340,6 @@ state_create (void)
     state->stop_turn_theta = 0.0;
     state->move = 's';
 
-    //state->vxworld = vx_world_create ();
-    //state->vxeh = calloc (1, sizeof(*state->vxeh));
-    //state->vxeh->key_event = key_event;
-    //state->vxeh->mouse_event = mouse_event;
-    //state->vxeh->touch_event = touch_event;
-    //state->vxeh->dispatch_order = 100;
-    //state->vxeh->impl = state; // this gets passed to events, so store useful struct here!
-
-    //state->vxapp.display_started = eecs467_default_display_started;
-    //state->vxapp.display_finished = eecs467_default_display_finished;
-    //state->vxapp.impl = eecs467_default_implementation_create (state->vxworld, state->vxeh);
-
     return state;
 }
 
@@ -347,14 +352,8 @@ state_destroy (state_t *state)
     if (state->lcm)
         lcm_destroy(state->lcm);
 
-    //if (state->vxeh)
-    //    free (state->vxeh);
-
     if (state->gopt)
         getopt_destroy (state->gopt);
-
-    //if (state->pg)
-    //    pg_destroy (state->pg);
 
     free (state);
 }
@@ -379,29 +378,66 @@ main (int argc, char *argv[])
         return 0;
     }
 
-    //vx_remote_display_source_t *cxn = vx_remote_display_source_create (&state->vxapp);
+	const zarray_t *args = getopt_get_extra_args(state->gopt);
+    if (zarray_size(args) > 0) {
+        zarray_get(args, 0, &state->url);
+    } else {
+        zarray_t *urls = image_source_enumerate();
 
-    //state->pg = pg_create ();
+        printf("Cameras:\n");
+        for (int i = 0; i < zarray_size(urls); i++) {
+            char *url;
+            zarray_get(urls, i, &url);
+            printf("  %3d: %s\n", i, url);
+        }
 
-    //parameter_listener_t *my_listener = calloc (1, sizeof(*my_listener));
-    //my_listener->impl = state;
-    //my_listener->param_changed = my_param_changed;
-    //pg_add_listener (state->pg, my_listener);
+        if (zarray_size(urls) == 0) {
+            printf("No cameras found.\n");
+            exit(0);
+        }
+        zarray_get(urls, 0, &state->url);
+    }
+
+	g_type_init();
+    gtk_init (&argc, &argv);
+    gdk_threads_init();
+
+    state->window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(state->window), state->url);
+
+    pthread_mutex_init(&state->mutex, NULL);
+    state->image = gtk_image_new();
+
+    gtk_container_add(GTK_CONTAINER(state->window), state->image);
+    gtk_widget_show(state->image);
+    gtk_widget_show(state->window);
+
+	state->isrc = image_source_open(state->url);
+    if (state->isrc == NULL) {
+        printf("Unable to open device %s\n", state->url);
+        exit(-1);
+    }
+
+    image_source_t *isrc = state->isrc;
+
+    if (isrc->start(isrc))
+        exit(-1);
+
+    state->fidx = isrc->get_current_format(isrc);
 
     change_need_scan(false, state);
 
     pthread_create (&state->lcm_thread, NULL, receive_lcm_msg, state);
     pthread_create (&state->drive_square_thread, NULL, drive_square_loop, state);
+	pthread_create (&state->cam_thread, NULL, cam_thread, state);
 
-    //eecs467_gui_run (&state->vxapp, state->pg, 768, 768);
+	gtk_main();
 
     pthread_join (state->lcm_thread, NULL);
     pthread_join (state->drive_square_thread, NULL);
+	pthread_join (state->cam_thread, NULL);
 
-    //free (my_listener);
     state_destroy (state);
-    //vx_remote_display_source_destroy (cxn);
-    //vx_global_destroy ();
 
 	return 0;
 }
